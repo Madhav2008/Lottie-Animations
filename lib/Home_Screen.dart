2e8039234b52ccf9ee221e6f91f3e216ec214f3d
arora <aroramadhav65@gmail.com>
2022-05-09 19:39:20 +0530

    'Update'

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
       
     );
   }
