1dfd71d2518f9ae114aa29a69a63831d1021313a
arora <aroramadhav65@gmail.com>
2022-05-09 19:43:44 +0530

    'Update'

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lottie Animations',
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
