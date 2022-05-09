3bed81532e6953a725a04e54c1dfe7930899b1c2
arora <aroramadhav65@gmail.com>
2022-05-09 20:19:45 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -32,6 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           Lottie.network(
             'https://assets10.lottiefiles.com/packages/lf20_dwmb4mrt.json',
+            width: 50,
           ),
         ],
       ),
