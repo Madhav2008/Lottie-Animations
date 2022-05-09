c48ce7862742fdf88ca515749836212fa0bb1f65
arora <aroramadhav65@gmail.com>
2022-05-09 19:41:34 +0530

    'Update'

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
